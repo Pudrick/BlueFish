eb38f57c71b7991d9a0bbf8e3828091a33b1597b
Pudrick <Pudrick@outlook.com>
2024-01-16 17:37:26 +0800

    changed to mobile API

--- lib/thread_list.dart
+++ lib/thread_list.dart
@@ -1,95 +1,75 @@
-import 'package:html/dom.dart';
+import 'dart:convert';
+
+import 'single_thread_title.dart';
 import 'package:http/http.dart' as http;
-import 'package:html/parser.dart' as html;
-import 'single_thread.dart';
 
-const bbsUrl = 'https://bbs.hupu.com';
-const topicIDHI3 = 788;
+class ThreadTitleList {
+  late List<SingleThreadTitle> threadTitleList = List.empty(growable: true);
 
-class ThreadList {
-  List<SingleThread> threadList = List.empty(growable: true);
+  int topicID = 788;
 
-  var page = 1;
-  var sort = "";
+  /// 0 => main topic
+  int zoneID = 0;
+  int page = 1;
+  String appVersion = "8.0.68";
 
-  void refresh() async {
-    var elementList = listRequest();
-    parseList(await elementList);
+  /// same as tab_type, 2 for newest repy, 1 for newest publish, 4 for 24h rank, 3 for essenses
+  int pickMethod = 2;
+
+  // ThreadTitleList();
+  ThreadTitleList.defaultList();
+  ThreadTitleList(this.topicID, this.zoneID, this.page, this.pickMethod);
+
+  Uri baseURL() {
+    return Uri.parse(
+        "https://bbs.mobileapi.hupu.com/1/$appVersion/topics/getTopicThreads?");
   }
 
-  Future<List<Element>> listRequest() async {
-    var fullUrl = '$bbsUrl/$topicIDHI3-$page';
-    var url = Uri.parse(fullUrl);
-    var response = await http.get(url);
-    var dom = html.parse(response.body);
-    var divList = dom.querySelectorAll('.bbs-sl-web-post-layout');
-    return divList;
+  Uri pinnedURL() {
+    return Uri.parse(
+        "https://bbs.mobileapi.hupu.com/1/$appVersion/topics/$topicID");
   }
 
-  void parseList(List<Element> divList) {
-    if (divList[0].className != "bbs-sl-web-post-layout") divList.removeAt(0);
-    //remove the first node contains no threads
-    for (var element in divList) {
-      parseThreadElement(element);
+  Uri generateURL() {
+    String baseurl = baseURL().toString();
+    var res = "$baseurl&topic_id=$topicID&tab_type=$pickMethod";
+    if (zoneID == 0) {
+      return Uri.parse(res);
+    }
+    return Uri.parse("$res&zoneId=$zoneID");
+  }
+
+  void toNextPage() {
+    page++;
+  }
+
+  Future<void> getPinnedThreads() async {
+    var jsonPinned = await http.get(pinnedURL());
+    var mappedThreads = jsonDecode(jsonPinned.body);
+    var pinnedList = mappedThreads["data"]["topicTopList"];
+    for (var Thread in pinnedList) {
+      SingleThreadTitle pinThead = SingleThreadTitle.fromJson(Thread);
+      pinThead.isPinned = true;
+      threadTitleList.add(pinThead);
     }
   }
 
-  void parseThreadElement(Element threadElement)
-  // div title
-  //    a link
-  //    svg light
-  //    pagelist
-  // div reply/skim
-  // div author
-  //    a link to author
-  // div time
-  {
-    SingleThread newThread = SingleThread();
-    threadList.add(newThread);
-    parseTitleDiv(threadElement.getElementsByClassName('post-title'));
-    parseReplyReadDiv(threadElement.getElementsByClassName("post-datum"));
-    parseAuthorDiv(threadElement.getElementsByClassName("post-auth"));
-    parsePosttimeDiv(threadElement.getElementsByClassName("post-time"));
-  }
-
-  void parseTitleDiv(List<Element> parentTitleDivList) {
-    // the length of titleDivList is 1
-    assert(parentTitleDivList.length == 1);
-    var titleDivList = parentTitleDivList[0].children;
-    for (var element in titleDivList) {
-      if (element.className == 'light-icon') {
-        threadList.last.isLighted = true;
-        continue;
-      }
-      if (element.className == 'page-icon') {
-        threadList.last.hasPageList = true;
-        continue;
-      }
-      threadList.last.title = element.text;
-      var threadID = element.attributes["href"];
-      threadList.last.titleLink = Uri.parse("$bbsUrl$threadID");
+  Future<void> getNormalThreads() async {
+    var URL = generateURL();
+    var jsonThreads = await http.get(URL);
+    var mappedthreads = jsonDecode(jsonThreads.body);
+    var threadList = mappedthreads["data"]["list"];
+    for (var Thread in threadList) {
+      SingleThreadTitle newThread = SingleThreadTitle.fromJson(Thread);
+      threadTitleList.add(newThread);
     }
   }
 
-  void parseReplyReadDiv(List<Element> parentRRDivList) {
-    // the length of List is always 1
-    assert(parentRRDivList.length == 1);
-    var replyRead = parentRRDivList.first.text.split('/');
-    var reply = replyRead[0];
-    var read = replyRead[1];
-    threadList.last.reply = int.parse(reply);
-    threadList.last.read = int.parse(read);
-  }
-
-  void parseAuthorDiv(List<Element> parentAuthorList) {
-    assert(parentAuthorList.length == 1);
-    var authorDiv = parentAuthorList.first.firstChild;
-    threadList.last.author = authorDiv!.text!;
-    threadList.last.authorLink = Uri.parse(authorDiv.attributes["href"]!);
-  }
-
-  void parsePosttimeDiv(List<Element> parentPosttimeList) {
-    assert(parentPosttimeList.length == 1);
-    threadList.last.postTime = parentPosttimeList.first.text;
+  Future<void> refresh() async {
+    print('1');
+    await getPinnedThreads();
+    print('2');
+    await getNormalThreads();
+    print('3');
   }
 }
