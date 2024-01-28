d80ddf7f29d373b0f8aace1db8312dc2bfc88294
Pudrick <Pudrick@outlook.com>
2024-01-28 17:48:41 +0800

    nothing changed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'title_list_page.dart';
+import 'thread_list_page.dart';
 import 'theme_settings.dart';
 
 void main() {
