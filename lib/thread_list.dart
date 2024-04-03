0a83fb061e105394af3feaea189ee865c074f390
Pudrick <Pudrick@outlook.com>
2024-04-03 17:06:00 +0800

    refactored refresh in declaration style, and added theater filter.

--- lib/thread_list.dart
+++ lib/thread_list.dart
@@ -1,25 +1,54 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
+
 import 'single_thread_title.dart';
 import 'package:http/http.dart' as http;
 
-class ThreadTitleList {
+int mainTopicID = 788;
+
+int mainZoneID = 0;
+int theaterZoneID = 253;
+
+/// position 0 just for index offset.
+/// as tab_type, 2 for newest reply, 1 for newest publish, 4 for 24h rank, 3 for essences
+enum SortType { sortType0Position, newestPublish, newestReply, essences, rank }
+
+class ThreadTitleList extends ChangeNotifier {
   late List<SingleThreadTitle> threadTitleList = List.empty(growable: true);
 
-  int topicID = 788;
+  int topicID = mainTopicID;
   int fid = 4875;
 
   /// 0 => main topic
-  int zoneID = 0;
+  int zoneID = mainZoneID;
+  void setZoneID(int newZone) {
+    zoneID = newZone;
+    refresh();
+  }
+
   int page = 1;
   String appVersion = "8.0.68";
 
-  /// same as tab_type, 2 for newest repy, 1 for newest publish, 4 for 24h rank, 3 for essenses
-  int pickMethod = 2;
+  bool isRefreshing = false;
+
+  SortType sortType = SortType.newestReply;
+
+  void setSortType(SortType newType) {
+    sortType = newType;
+    refresh();
+  }
 
   // ThreadTitleList();
-  ThreadTitleList.defaultList();
-  ThreadTitleList(this.topicID, this.zoneID, this.page, this.pickMethod);
+  ThreadTitleList.defaultList() {
+    refresh();
+  }
+  ThreadTitleList(this.topicID, this.zoneID, this.page, this.sortType)
+      : assert(sortType.index != 0) {
+    refresh();
+  }
+
+  //just for main page thread sort refresh
 
   Uri baseURL() {
     return Uri.parse(
@@ -32,8 +61,10 @@ class ThreadTitleList {
   }
 
   Uri generateURL() {
+    assert(sortType.index != 0);
     String baseurl = baseURL().toString();
-    var res = "$baseurl&topic_id=$topicID&tab_type=$pickMethod";
+    int tab_type = sortType.index;
+    var res = "$baseurl&topic_id=$topicID&tab_type=$tab_type";
     if (zoneID == 0) {
       return Uri.parse(res);
     }
@@ -42,14 +73,15 @@ class ThreadTitleList {
 
   void toNextPage() {
     page++;
+    notifyListeners();
   }
 
   Future<void> getPinnedThreads() async {
     var jsonPinned = await http.get(pinnedURL());
     var mappedThreads = jsonDecode(jsonPinned.body);
     var pinnedList = mappedThreads["data"]["topicTopList"];
-    for (var Thread in pinnedList) {
-      SingleThreadTitle pinThead = SingleThreadTitle.fromJson(Thread);
+    for (var thread in pinnedList) {
+      SingleThreadTitle pinThead = SingleThreadTitle.fromJson(thread);
       pinThead.isPinned = true;
       threadTitleList.add(pinThead);
     }
@@ -60,14 +92,23 @@ class ThreadTitleList {
     var jsonThreads = await http.get(URL);
     var mappedthreads = jsonDecode(jsonThreads.body);
     var threadList = mappedthreads["data"]["list"];
-    for (var Thread in threadList) {
-      SingleThreadTitle newThread = SingleThreadTitle.fromJson(Thread);
-      threadTitleList.add(newThread);
+    for (var thread in threadList) {
+      SingleThreadTitle newThread = SingleThreadTitle.fromJson(thread);
+      if (zoneID != 253 && newThread.zoneId != 253) {
+        threadTitleList.add(newThread);
+      } else if (zoneID == 253 && newThread.zoneId == 253) {
+        threadTitleList.add(newThread);
+      }
     }
   }
 
-  Future<void> refresh() async {
+  void refresh() async {
+    isRefreshing = true;
+    notifyListeners();
+    threadTitleList.clear();
     await getPinnedThreads();
     await getNormalThreads();
+    isRefreshing = false;
+    notifyListeners();
   }
 }
