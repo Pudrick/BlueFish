f71280d5e7d049ed9215b077b1f4f72f72ee890e
Pudrick <Pudrick@outlook.com>
2024-09-06 00:08:41 +0800

    changed http request format, use map to parse thread.

---
+++ lib/utils/http_with_ua.dart
@@ -0,0 +1,16 @@
+import 'package:http/http.dart' as http;
+
+const String chromeUserAgent =
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36";
+
+class HttpwithUA extends http.BaseClient {
+  final http.Client _inner = http.Client();
+
+  HttpwithUA();
+
+  @override
+  Future<http.StreamedResponse> send(http.BaseRequest request) {
+    request.headers['user-agent'] = chromeUserAgent;
+    return _inner.send(request);
+  }
+}
