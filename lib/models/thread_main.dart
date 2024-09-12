8ac89636d0a2c9b5aaa79435ac766edb4ed542a5
Pudrick <Pudrick@outlook.com>
2024-09-12 14:17:53 +0800

    changed file structure again...

--- lib/models/thread_main.dart
+++ lib/models/thread_main.dart
@@ -1,1 +1,12 @@
-class ThreadMain {}
+import 'author.dart';
+
+class ThreadMain {
+  late int tid;
+  late String title;
+  late Author author;
+  late int postDateTimeStamp;
+  late String postDateTimeString; // in fact can be find from timestamp
+  late String postLocation;
+  late String sourceClient;
+  late String contentHTML;
+}
