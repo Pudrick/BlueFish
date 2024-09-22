57513aeb526853aca162902b1a0bdbda50fc6dde
Pudrick <Pudrick@outlook.com>
2024-09-23 00:07:13 +0800

    tentatively tested replies requirements. start working with thread detail widgets and pages.

---
+++ lib/widgets/thread_main_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import '../models/thread_detail.dart';
+
+class ThreadMainFloorWidget extends StatelessWidget {
+  const ThreadMainFloorWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {}
+}
