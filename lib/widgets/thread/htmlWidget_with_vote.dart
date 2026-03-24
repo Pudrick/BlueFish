7d9abaec0cbb04b174b4af80763fa16042467f09
Pudrick <Pudrick@outlook.com>
2026-03-24 22:21:31 +0800

    feat(vibe): wrap HtmlWidgetWithVote in SelectionArea

--- lib/widgets/thread/htmlWidget_with_vote.dart
+++ lib/widgets/thread/htmlWidget_with_vote.dart
@@ -3,16 +3,18 @@ import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 import 'package:bluefish/widgets/vote/vote_widget.dart';
 
 Widget HtmlWidgetWithVote(String htmlStr, {TextStyle? textStyle}) {
-  return HtmlWidget(
-    htmlStr,
-    textStyle: textStyle,
-    customWidgetBuilder: (element) {
-      if (element.localName == "span" &&
-          element.attributes["data-type"] == "vote") {
-        final String voteID = element.attributes["data-vote-id"]!;
-        return VoteWidget(voteID: int.parse(voteID));
-      }
-      return null;
-    },
+  return SelectionArea(
+    child: HtmlWidget(
+      htmlStr,
+      textStyle: textStyle,
+      customWidgetBuilder: (element) {
+        if (element.localName == "span" &&
+            element.attributes["data-type"] == "vote") {
+          final String voteID = element.attributes["data-vote-id"]!;
+          return VoteWidget(voteID: int.parse(voteID));
+        }
+        return null;
+      },
+    ),
   );
 }
